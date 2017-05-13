d87571b74e69e6840fc719219248461cb95c69b0
Michael <tng.michael@yahoo.com>
2017-05-13 10:12:51 +0700

    Solve conflict

--- dofin-backend/server.js
+++ dofin-backend/server.js
@@ -2,13 +2,17 @@ const express     = require('express'),
       app         = express(),
       bodyParser  = require('body-parser'),
       cors        = require('cors');
+
 const income = require('./routes/income');
 const dream = require('./routes/dream');
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
 app.use("/income", income);
 app.use("/dream", dream);
+
 app.listen(3001 || process.env.PORT, () => {
   console.log(`Server running!`);
 })
