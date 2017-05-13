603b58645cbb477302757ff2942623cb6ad87ba0
Michael <tng.michael@yahoo.com>
2017-05-13 11:22:53 +0700

    Change models

--- dofin-backend/server.js
+++ dofin-backend/server.js
@@ -2,14 +2,6 @@ const express     = require('express'),
       app         = express(),
       bodyParser  = require('body-parser'),
       cors        = require('cors'),
-      mongoose    = require('mongoose');
-
-// let host = 'mongodb://tngmichael:1234cluster@cluster0-shard-00-00-ldzrg.mongodb.net:27017,cluster0-shard-00-01-ldzrg.mongodb.net:27017,cluster0-shard-00-02-ldzrg.mongodb.net:27017/dofin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
-//
-// // host = 'mongodb://localhost:27017/dofin'
-// mongoose.Promise = global.Promise
-// mongoose.connect(host, err =>
-//   err === undefined ? console.log('DB connected'):console.log('ERROR'))
 
 const income = require('./routes/income');
 const dream = require('./routes/dream');
