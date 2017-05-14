6d09f3debe3f88c6d51e8e1d2901064bbf7b844c
primayudhamanggala <primayudha.manggala@gmail.com>
2017-05-14 16:29:59 +0700

    update struk

--- dofin-backend/routes/expense.js
+++ dofin-backend/routes/expense.js
@@ -8,5 +8,6 @@ router.get("/:user_id/total_amount_by_month", controller.getTotalAmountByMonthBy
 router.get("/:user_id/total_amount_by_category", controller.getTotalAmountByCategoryThisYearById)
 router.post("/", controller.newExpense)
 router.delete("/:id", controller.removeExpenseById)
+router.post("/photo", controller.newPhoto)
 
 module.exports = router
