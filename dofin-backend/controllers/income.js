8e71a90ea25a6ce6b1504f0c394a2f264b05f3f6
Michael <tng.michael@yahoo.com>
2017-05-14 11:52:53 +0700

    Add aggregate total by month and category (income and expense)

--- dofin-backend/controllers/income.js
+++ dofin-backend/controllers/income.js
@@ -41,6 +41,64 @@ const newIncome = (req, res) => {
   })
 } // newIncome
 
+const getTotalAmountByMonthById = (req, res) => {
+  Income.aggregate(
+    [
+      {$project: {
+        month: { $month: "$date" },
+        year: { $year: "$date" },
+        amount: 1, // or
+        record_by: 1,
+      }},
+      {$match: {
+        record_by: mongoose.Types.ObjectId(req.params.user_id),
+      }},
+      {$group: {
+        _id: { year: '$year', month: '$month' },
+        total_amount: { $sum: "$amount" },
+      }},
+      {$group: {
+        _id: { year: '$_id.year' },
+        months: { $addToSet: { month: "$_id.month", total_amount: "$total_amount" } },
+      }},
+    ])
+      .exec((err, rec) => {
+        if (err) res.send(err)
+        else res.json(rec)
+      })
+} // getTotalAmountByMonthById
+
+const getTotalAmountByCategoryThisYearById = (req, res) => {
+  const year = new Date().getFullYear()
+  Income.aggregate(
+    [
+      {$project: {
+        month: { $month: "$date" },
+        year: { $year: "$date" },
+        category: '$category',
+        amount: 1, // or
+        record_by: 1,
+      }},
+      {$match: {
+        record_by: mongoose.Types.ObjectId(req.params.user_id),
+        year
+      }},
+      {$group: {
+        _id: { month: '$month', category: '$category' },
+        total_amount: { $sum: "$amount" },
+      }},
+      {$group: {
+        _id: { month: '$_id.month' },
+        total_amount: { $sum: "$total_amount" },
+        categories: { $addToSet: { category: "$_id.category", total_amount: "$total_amount" } },
+      }},
+    ])
+      .exec((err, rec) => {
+        if (err) res.send(err)
+        else res.json(rec)
+      })
+} // getTotalAmountByCategoryThisYearById
+
 const removeIncomeById = (req, res) => {
   Income.findByIdAndRemove(req.params.id)
     .exec((err, rec) => {
@@ -52,6 +110,8 @@ const removeIncomeById = (req, res) => {
 module.exports = {
   getIncomeById,
   getTotalAmountByUserId,
+  getTotalAmountByMonthById,
+  getTotalAmountByCategoryThisYearById,
   newIncome,
   removeIncomeById,
 }
--- dofin-backend/routes/income.js
+++ dofin-backend/routes/income.js
@@ -4,6 +4,8 @@ const express     = require('express'),
 
 router.get("/:user_id", controller.getIncomeById)
 router.get("/:user_id/total_amount", controller.getTotalAmountByUserId)
+router.get("/:user_id/total_amount_by_month", controller.getTotalAmountByMonthById)
+router.get("/:user_id/total_amount_by_category", controller.getTotalAmountByCategoryThisYearById)
 router.post("/", controller.newIncome)
 router.delete("/:id", controller.removeIncomeById)
 
